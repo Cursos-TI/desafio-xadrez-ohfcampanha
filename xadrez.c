e25512f29bef08d389266a8d63acb98b20f4eed7
ohfcampanha <ohfcampanha@gmail.com>
2025-10-18 19:15:49 +0000

    Commit final do projeto xadrez

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,41 @@
 #include <stdio.h>
+//Movimentação das peças//
+void torre (int casas){
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
-
+    if (casas > 0) {
+        printf(" Torre Direita\n");
+        torre (casas - 1);
+    }
+}
+void rainha (int casas){
+    if (casas > 0) {
+        printf("Rainha Esquerda\n");
+        rainha (casas - 1);
+    }
+}
+void bispo (int casas){
+    if (casas > 0) {
+        
+        printf("Bispo Cima\n");
+        printf("Bispo Direita\n"); 
+        
+       
+        bispo(casas - 1); 
+    }
+}
+void cavalo (int casas){
+    if (casas > 0) {
+        for (int c = 1; c <= 2; c++){
+            printf("Cavalo Cima\n");
+        }
+        printf("Cavalo Direita\n");
+    }
+}
+//Chamada das funções//
+int main (){
+    bispo (5);
+    torre (5);
+    rainha (8);
+    cavalo (1);
     return 0;
 }
